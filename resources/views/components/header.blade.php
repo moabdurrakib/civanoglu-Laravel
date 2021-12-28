c8e6e954268dcfab8dca624615dd3b63a54f1a33
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-12-29 02:11:51 +0600

    header section , menu completed

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30">
+    <div class="min-w-max">
+        <img width="100" src="{{asset('image/logomain.png')}}" alt="">
+    </div>
+
+    <div class="w-full">
+        <ul class="flex justify-center">
+            <li> <a class="inline-block px-4 text-white" href="">Land</a> </li>
+            <li> <a class="inline-block px-4 text-white" href="">Villa</a> </li>
+            <li> <a class="inline-block px-4 text-white" href="">Apartment</a> </li>
+            <li> <a class="inline-block px-4 text-white" href="">About Us</a> </li>
+            <li> <a class="inline-block px-4 text-white" href="">Contact Us</a> </li>
+        </ul>
+    </div>
+    <div class="min-w-max text-3xl">
+        <a class="text-white " href="">🇹🇷</a>
+        <a class="text-white " href="">🇺🇸</a>
+    </div>
+
+
+</div>
